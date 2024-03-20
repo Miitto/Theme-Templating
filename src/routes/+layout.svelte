96c63efc56a0600ac1b4aca93376f7ac8ada408a
Miitto <wpbickerton@gmail.com>
2024-03-20 15:26:57 +0000

    Heatmap Theme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../style.scss';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		height: 100vh;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	import ExampleBlock from '$lib/Components.svelte/ExampleBlock.svelte';
+</script>
+
+<div class="wrapper">
+	<div class="light">
+		<ExampleBlock theme="heatmap__light" />
+	</div>
+	<div class="transition"></div>
+	<div class="dark">
+		<ExampleBlock theme="heatmap__dark" />
+	</div>
+</div>
+
+<style lang="scss">
+	$t: 2rem;
+	.wrapper {
+		display: grid;
+		grid-template-columns: 1fr $t 1fr;
+		justify-content: center;
+		height: 100%;
+	}
+
+	.light,
+	.dark {
+		padding: 1rem;
+	}
+
+	.transition {
+		background: linear-gradient(to right, #f0f0f0, #000);
+		width: $t;
+	}
+
+	.light {
+		background-color: #f0f0f0;
+	}
+	.dark {
+		background-color: #000;
+	}
+</style>
